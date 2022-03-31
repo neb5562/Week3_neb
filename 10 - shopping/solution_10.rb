8f276b907fa08a9012cf57d344a030958451503d
neb5562 <gnebieridzs@unisens.ge>
2022-03-31 12:46:53 +0400

    changed

--- 10 - shopping/solution_10.rb
+++ 10 - shopping/solution_10.rb
@@ -8,13 +8,13 @@ class Shopping
     @shopping_total = 0.00
   end
   
-  def get_user_input
+  def start
     while true
-      puts "Product Name: (please enter 'stop' to end shopping.)"
+      puts "Product Name:"
       product_name = gets.chomp.to_s
 
       if product_name == "stop"
-        display_resultgit 
+        display_result
         break 
       end
       
@@ -33,38 +33,18 @@ class Shopping
 
   # display final shopping summary
   def display_result
-    item_number = 1
-    print_header
-    if !@items.empty?
-      @items.each do | item_key, item_value |
-        print_item item_number, item_key, item_value
-        item_number += 1
-        @shopping_total += (item_value["product_quantity"] * item_value["product_price"])
-      end
-    else
-      puts "Shopping cart is empty!"
-    end
-    print_footer 
-  end
-
-  private
-  def print_header
     puts "\nShopping Summary:\n"
     puts "-----------------\n"
-  end
-
-  def print_item item_number, item_key, item_value
-    print "##{item_number} - #{item_key}"
-    print " | Quantity: #{item_value["product_quantity"]}"
-    print " | Item Price: #{item_value["product_price"]}"
-    print " | Item Total Price: #{ (item_value["product_quantity"].to_f * item_value["product_price"].to_f).round(PRICE_ROUND_PRECISION) }\n"
-  end
-
-  def print_footer
+    item_number = 1
+    @items.each do | item_key, item_value |
+      puts "##{item_number} - #{item_key} Quantity: #{item_value["product_quantity"]}, Item Price: #{item_value["product_price"]}, Item Total Price: #{ (item_value["product_quantity"].to_f * item_value["product_price"].to_f).round(PRICE_ROUND_PRECISION) }\n"
+      item_number += 1
+      @shopping_total += (item_value["product_quantity"] * item_value["product_price"])
+    end
     puts "-----------------\n"
     puts "Total Shopping Price: #{@shopping_total.round(PRICE_ROUND_PRECISION)}"
   end
 end
 
-shopping = Shopping.new
-shopping.get_user_input
+shop = Shopping.new
+shop.start
