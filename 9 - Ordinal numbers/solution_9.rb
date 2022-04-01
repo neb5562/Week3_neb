906c14c5c809f2de5a751f0eb8c142025f91c540
neb5562 <gnebieridzs@unisens.ge>
2022-04-01 10:38:25 +0400

    Added validation

--- 9 - Ordinal numbers/solution_9.rb
+++ 9 - Ordinal numbers/solution_9.rb
@@ -2,12 +2,21 @@ require 'date'
 
 class OrdinalNum
   def initialize(year, month, day)
-    @year, @month, @day = year, month, day
+    @year, @month, @day = validate_int(year), validate_int(month), validate_int(day)
   end
 
   def get_ordinal_number
     "Day #{Date.new(@year, @month, @day).yday}."
   end
+
+  private
+  def validate_int(value)
+    if value.to_s.match(/^\d+$/)
+      value.to_i
+    else
+      raise StandardError.new("#{value} must be Integer value!")
+    end
+  end
 end
 
-puts OrdinalNum.new(2022, 3, 12).get_ordinal_number
+puts OrdinalNum.new("1999", 3, 12).get_ordinal_number
