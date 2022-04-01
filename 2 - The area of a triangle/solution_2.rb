2c2f932e7d798fb3dd9bdef5d403af814ef6e9ac
neb5562 <gnebieridzs@unisens.ge>
2022-04-01 15:50:06 +0400

    add validation class to task 2

--- 2 - The area of a triangle/solution_2.rb
+++ 2 - The area of a triangle/solution_2.rb
@@ -1,10 +1,16 @@
+require "./validation"
 TRIANGLE_AREA_ONE = 1
 TRIANGLE_AREA_TWO = 2
 
 class Triangle
   def initialize(base, height)
+    @validation = Validation.new
     @base = base.to_f
+    @validation.validate_presence_of(base, "base")
+    @validation.validate_float(base, "base")
     @height = height.to_f
+    @validation.validate_presence_of(height, "height")
+    @validation.validate_float(height, "height")
   end
   
   def get_area_of_triangle
