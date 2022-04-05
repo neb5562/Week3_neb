31e63017250932282954ee5ce4d84ef451911265
neb5562 <gnebieridzs@unisens.ge>
2022-04-05 15:13:44 +0400

    improved validation in task 2, same as task 1

--- 2 - The area of a triangle/solution_2.rb
+++ 2 - The area of a triangle/solution_2.rb
@@ -1,3 +1,4 @@
+class AttributeError < StandardError; end
 class Triangle
   require "./validation"
   TRIANGLE_AREA_ONE = 1
@@ -6,16 +7,15 @@ class Triangle
   def initialize(base, height)
     @validation = Validation.new  
     @validation.validate_presence(base, "base")
-    @validation.validate_type(base, Float, "base")
+    @validation.validate_number(base, "base")
     @validation.validate_presence(height, "height")
-    @validation.validate_type(height, Float, "height")
-    @validation.check_all_validations
-    @base = base.to_f
-    @height = height.to_f
+    @validation.validate_number(height, "height")
+    @base = base
+    @height = height
   end
   
   def get_area_of_triangle
-    @base * @height * TRIANGLE_AREA_ONE / TRIANGLE_AREA_TWO
+    @base.to_f * @height.to_f * TRIANGLE_AREA_ONE / TRIANGLE_AREA_TWO
   end
 end
 
--- 2 - The area of a triangle/solution_2_service.rb
+++ 2 - The area of a triangle/solution_2_service.rb
@@ -1,3 +1,4 @@
+class AttributeError < StandardError; end
 class Triangle
   require "./validation"
   TRIANGLE_AREA_ONE = 1
@@ -5,22 +6,27 @@ class Triangle
 
   def initialize(base, height)
     @validation = Validation.new  
-    @validation.validate_presence(base, "base")
-    @validation.validate_type(base, Float, "base")
-    @validation.validate_presence(height, "height")
-    @validation.validate_type(height, Float, "height")
-    @validation.check_all_validations
-    @base = base.to_f
-    @height = height.to_f
+    @base = base
+    @height = height
   end
   
   def call 
+    proceed_validations
     get_area_of_triangle
+  rescue AttributeError => e
+    warn e
   end
 
   private
+  def proceed_validations
+    @validation.validate_presence(@base, "base")
+    @validation.validate_number(@base, "base")
+    @validation.validate_presence(@height, "height")
+    @validation.validate_number(@height, "height")
+  end
+
   def get_area_of_triangle
-    @base * @height * TRIANGLE_AREA_ONE / TRIANGLE_AREA_TWO
+    @base.to_f * @height.to_f * TRIANGLE_AREA_ONE / TRIANGLE_AREA_TWO
   end
 end
 
