5b593f5e4657bf800aef13b2df3957aeba564ba4
neb5562 <gnebieridzs@unisens.ge>
2022-04-05 15:01:05 +0400

    added exceptions, improved validation class

--- 1-Ideal Weight/solution_1_service.rb
+++ 1-Ideal Weight/solution_1_service.rb
@@ -1,23 +1,29 @@
+class AttributeError < StandardError; end
 class Person
   require './validation'
   FIXED_VALUE_TO_SUBSCRAPT = 110
   
   def initialize(name, height)
     @validation = Validation.new
-    @validation.validate_presence(name, "name")
-    @validation.validate_presence(height, "height")
-    @validation.validate_type(height, Integer, "height")
-    @validation.validate_range(height, "height", 0)
-    @validation.check_all_validations
     @height = height
     @name = name
   end
 
   def call 
+    proceed_validations
     get_ideal_weight
+  rescue AttributeError => e
+    warn e
   end
   
   private
+  def proceed_validations
+    @validation.validate_presence(@name, "name")
+    @validation.validate_presence(@height, "height")
+    @validation.validate_number(@height, "height")
+    @validation.validate_range(@height, "height", 0)
+  end
+
   def get_ideal_weight
     @ideal_weight = @height.to_i - FIXED_VALUE_TO_SUBSCRAPT
     'Your weight is already optimal' if @ideal_weight.negative?
@@ -31,5 +37,5 @@ name = gets.chomp
 puts 'Please enter your height:'
 height = gets.chomp
 
-person = Person.new name, height
+person = Person.new(name, height)
 puts person.call
