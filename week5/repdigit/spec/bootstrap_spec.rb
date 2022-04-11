328cdfd819ea450497242a9af3be626312f37b81
neb5562 <gnebieridzs@unisens.ge>
2022-04-11 17:36:09 +0400

    improved tests with faker and best practiceS

--- week5/repdigit/spec/bootstrap_spec.rb
+++ week5/repdigit/spec/bootstrap_spec.rb
@@ -1,69 +1,50 @@
 require 'repdigit'
+require 'faker'
+SYMBOLS_NUMBER_FROM = 1
+SYMBOLS_NUMBER_TO = 999
 
-describe RepDigit do
-  it "returns true" do
-    expect(RepDigit.new(1).call).to equal true
+NUMBER_CHAR_FROM = 0
+NUMBER_CHAR_TO = 9
+
+GENERATE_MIN = 1
+GENERATE_MAX = 1_000_000
+
+STRING_LEN_FROM = 1
+STRING_LEN_TO = 30
+
+100.times do
+  describe RepDigit do
+    # must raise AttributeError fo ar negative numbers as it will not pass validation
+    it "raises AttributeError" do
+      expect{RepDigit.new(Faker::Number.negative).call}.to raise_error(AttributeError)
+    end
+
+    # will generate random numbers like 3333333, 555555555555555, 22222222. all msut be true
+    it "returns true" do
+      expect(RepDigit.new(Faker::Number.within(range: NUMBER_CHAR_FROM..NUMBER_CHAR_TO).to_s * rand(SYMBOLS_NUMBER_FROM...SYMBOLS_NUMBER_TO).to_i).call).to be_truthy
+    end
+
+    # need to generate non repdigit numbers
+    it "returns false" do
+      expect(RepDigit.new(Faker::Number.between(from: GENERATE_MIN, to: GENERATE_MAX).to_i).call).to be_falsey
+    end
+    # will generate random string
+    it "raises AttributeError" do
+      expect{ RepDigit.new(Faker::String.random(length: STRING_LEN_FROM..STRING_LEN_TO)).call }.to raise_error(AttributeError)
+    end
+
   end
 end
 
+# empty value or nil
 describe RepDigit do
-  it "returns false" do
-    expect(RepDigit.new(10).call).to equal false
-  end
-end
 
-
-describe RepDigit do
-  it "returns true" do
-    expect(RepDigit.new('555').call).to equal true
-  end
-end
-
-describe RepDigit do
-  it "returns true" do
-    expect(RepDigit.new(9999999999999999).call).to equal true
-  end
-end
-
-describe RepDigit do
-  it "returns true" do
-    expect(RepDigit.new('9999999999999999').call).to equal true
-  end
-end
-
-
-describe RepDigit do
-  it "returns true" do
-    expect(RepDigit.new(555).call).to equal true
-  end
-end
-
-describe RepDigit do
-  it "returns false" do
-    expect(RepDigit.new(4555).call).to equal false
-  end
-end
-
-describe RepDigit do
-  it "raises AttributeError" do
-    expect{ RepDigit.new('4555a').call }.to raise_error(AttributeError)
-  end
-end
-
-describe RepDigit do
   it "raises AttributeError" do
     expect{ RepDigit.new('').call }.to raise_error(AttributeError)
   end
-end
 
-describe RepDigit do
   it "raises AttributeError" do
-    expect{ RepDigit.new(-1).call }.to raise_error(AttributeError)
+    expect{ RepDigit.new(nil).call }.to raise_error(AttributeError)
   end
-end
 
-describe RepDigit do
-  it "raises AttributeError" do
-    expect{ RepDigit.new('asfasf43534').call }.to raise_error(AttributeError)
-  end
 end
