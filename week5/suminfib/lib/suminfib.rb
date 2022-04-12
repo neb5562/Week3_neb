6777a1f5cd23cf96db9ddd96f9786741768a9729
neb5562 <gnebieridzs@unisens.ge>
2022-04-12 11:22:09 +0400

    sum of fib numberS

---
+++ week5/suminfib/lib/suminfib.rb
@@ -0,0 +1,20 @@
+class SumInFib
+  def initialize(range_start, range_end)
+    @range_start = range_start
+    @range_End   = range_end
+  end
+
+  def call
+
+  end
+
+  private
+
+  def validate
+    
+  end
+
+  def sum
+
+  end
+end
