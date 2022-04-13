224dcc07e615a7d5bbc882cbda51943ab2cd47e7
neb5562 <gnebieridzs@unisens.ge>
2022-04-13 16:11:03 +0400

    added constants

--- week5/missingnum/lib/arrmissnum.rb
+++ week5/missingnum/lib/arrmissnum.rb
@@ -7,9 +7,14 @@ ARRAY_TEMPLATE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].freeze
 
 class ArrMissNum
   include Validation
-  MAX_NUMBER = 10
-  MIN_NUMBER = 1
-  USER_INPUT_ARR_SIZE = 9
+  MAX_NUMBER                = 10
+  MIN_NUMBER                = 1
+  USER_INPUT_ARR_SIZE       = 9
+  INPUT_OBJECT              = 'array'.freeze
+  NOT_ARRAY_MESSAGE         = 'must be array'.freeze
+  ARRAY_OF_INTEGERS_MESSAGE = 'must be array of integers'.freeze
+  ARRAY_SIZE_MESSAGE        = 'size of a array must be 9'.freeze
+  INVALID_ARRAY_MESSAGE     = 'invalid array'.freeze
 
   def initialize(input_array)
     @input_array = input_array 
@@ -27,11 +32,11 @@ class ArrMissNum
   private
 
   def validate_array
-    Validation.validate_presence(@input_array, "array")
-    Validation.validate_with_lambda(@input_array.kind_of?(Array), "must be array")
-    Validation.validate_with_lambda(@input_array.all?{ |num| num.is_a? Integer }, "must be array of integers")
-    Validation.validate_with_lambda(@input_array.length == USER_INPUT_ARR_SIZE, "size of a array must be 9")
-    Validation.validate_with_lambda(find_missing_num.between?(MIN_NUMBER,MAX_NUMBER), "invalid array")
+    Validation.validate_presence(@input_array, INPUT_OBJECT)
+    Validation.validate_with_lambda(@input_array.kind_of?(Array), NOT_ARRAY_MESSAGE)
+    Validation.validate_with_lambda(@input_array.all?{ |num| num.is_a? Integer }, ARRAY_OF_INTEGERS_MESSAGE)
+    Validation.validate_with_lambda(@input_array.length == USER_INPUT_ARR_SIZE, ARRAY_SIZE_MESSAGE)
+    Validation.validate_with_lambda(find_missing_num.between?(MIN_NUMBER,MAX_NUMBER), INVALID_ARRAY_MESSAGE)
   end
 
   def find_missing_num
