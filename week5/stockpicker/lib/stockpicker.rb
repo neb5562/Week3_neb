f12608922093b369c77395af89a75ae5855e0567
neb5562 <gnebieridzs@unisens.ge>
2022-04-14 09:19:57 +0400

    added some tests

--- week5/stockpicker/lib/stockpicker.rb
+++ week5/stockpicker/lib/stockpicker.rb
@@ -5,9 +5,11 @@ class AttributeError < StandardError; end
 require 'validation'
 
 class StockPicker
-  ARRAY_MIN_SIZE      = 2
-  EMPTY_ARRAY_MESSAGE = 'array must be not empty'.freeze
-  ARRAY_SIZE_MESSAGE  = 'array size must be over 1'.freeze
+  ARRAY_MIN_SIZE            = 2
+  EMPTY_ARRAY_MESSAGE       = 'array must be not empty'.freeze
+  ARRAY_SIZE_MESSAGE        = 'array size must be over 1'.freeze
+  ARRAY_OF_INTEGERS_MESSAGE = 'array must include only integers'.freeze
+
   include Validation
 
   def initialize(array)
@@ -28,6 +30,7 @@ class StockPicker
     Validation.validate_presence(@stock_array, 'array')
     Validation.validate_with_lambda(@stock_array.empty? == false, EMPTY_ARRAY_MESSAGE)
     Validation.validate_with_lambda(@stock_array.size < ARRAY_MIN_SIZE == false, ARRAY_SIZE_MESSAGE)
+    Validation.validate_with_lambda(@stock_array.all?{ |num| num.is_a? Integer }, ARRAY_OF_INTEGERS_MESSAGE)
   end
 
   def stock_picker
