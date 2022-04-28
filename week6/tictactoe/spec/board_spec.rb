91ca3a81b303eb497b69b32330f93fa8108870c0
neb5562 <gnebieridzs@unisens.ge>
2022-04-28 09:45:26 +0400

    finished all testing

--- week6/tictactoe/spec/board_spec.rb
+++ week6/tictactoe/spec/board_spec.rb
@@ -22,8 +22,6 @@ RSpec.describe Board do
     end
   end
 
-  it "#winner_line?"
-  it "validate winning combinations"
   context "#full?" do
     let(:full_board){ [[1, 1, 0], [0, 0, 1], [1, 0, 1]] }
     let(:not_full_board){ [[1, 1, 0], [nil, 0, 1], [1, 0, 1]] }
@@ -56,4 +54,75 @@ RSpec.describe Board do
       expect(subject.board).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
     end
   end
+
+  context "validate horizontal winning combinations" do
+    let(:winnng_horizontal_line_1){ [[1, nil, nil], [1, nil, nil], [1, nil, nil]] }
+    let(:winnng_horizontal_line_2){ [[nil, 1, nil], [nil, 1, nil], [nil, 1, nil]] }
+    let(:winnng_horizontal_line_3){ [[nil, nil, 1], [nil, nil, 1], [nil, nil, 1]] }
+    subject("board_one"){ described_class.new(winnng_horizontal_line_1) }
+    subject("board_two"){ described_class.new(winnng_horizontal_line_2) }
+    subject("board_three"){ described_class.new(winnng_horizontal_line_3) }
+
+    it "should give winning top horizontal line" do
+      expect(board_one.winner_line?).to eq(1)
+    end
+
+    it "should give winning middle horizontal line" do
+      expect(board_two.winner_line?).to eq(1)
+    end
+
+    it "should give winning bottom horizontal line" do
+      expect(board_three.winner_line?).to eq(1)
+    end
+  end
+
+  context "validate vertical winning combinations" do
+    let(:winnng_vertical_line_1){ [[1, nil, nil], [1, nil, nil], [1, nil, nil]] }
+    let(:winnng_vertical_line_2){ [[nil, 0, nil], [nil, 0, nil], [nil, 0, nil]] }
+    let(:winnng_vertical_line_3){ [[nil, nil, 1], [nil, nil, 1], [nil, nil, 1]] }
+    subject("board_one"){ described_class.new(winnng_vertical_line_1) }
+    subject("board_two"){ described_class.new(winnng_vertical_line_2) }
+    subject("board_three"){ described_class.new(winnng_vertical_line_3) }
+
+    it "should give winning first vertical line" do
+      expect(board_one.winner_line?).to eq(1)
+    end
+
+    it "should give winning middle vertical line" do
+      expect(board_two.winner_line?).to eq(0)
+    end
+
+    it "should give winning last vertical line" do
+      expect(board_three.winner_line?).to eq(1)
+    end
+  end
+
+  context "validate diagonal winning combinations" do
+    let(:winnng_diagonal_line_1){ [[1, nil, nil], [nil, 1, nil], [nil, nil, 1]] }
+    let(:winnng_diagonal_line_2){ [[nil, nil, 0], [nil, 0, nil], [0, nil, nil]] }
+    subject("board_one"){ described_class.new(winnng_diagonal_line_1) }
+    subject("board_two"){ described_class.new(winnng_diagonal_line_2) }
+
+    it "should give winning left diagonal line" do
+      expect(board_one.winner_line?).to eq(1)
+    end
+
+    it "should give winning right diagonal line" do
+      expect(board_two.winner_line?).to eq(0)
+    end
+  end
+
+  context "validate diagonal winning combinations" do
+    let(:no_win_1){ [[1, 0, 1], 
+                    [1, 0, 0], 
+                    [0, 1, 1]] }
+
+    subject("board_one"){ described_class.new(no_win_1) }
+
+    it "not winning combination 1" do
+      expect(board_one.winner_line?).to be_falsey
+    end
+
+  end
+
 end
