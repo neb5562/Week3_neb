25339f7f1d83c97d08cfa0a52b2075f324f0d1ea
neb5562 <gnebieridzs@unisens.ge>
2022-05-18 17:19:43 +0400

    finished without rspec

--- week9/todo/app/controllers/application_controller.rb
+++ week9/todo/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require 'date'
+require 'cgi'
 
 class ApplicationController < Sinatra::Base
   configure do
@@ -11,10 +12,29 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    @list ||= logged_in? ? current_user.todos.all().order(Arel.sql("(deadline - now()) asc")) : nil
+    @per_page = 6
+    @page = params['page'].to_i % @per_page == 0 ? params['page'].to_i : 0
+    @list ||= logged_in? ? current_user.todos.offset(@page).limit(@per_page).where("deadline > now()").where(is_done: false).order(Arel.sql("is_done desc, (deadline - now()) asc")) : nil
+    @count = @list.count
+    erb :'list/index'
+  end
+
+  get "/finished" do
+    @per_page = 6
+    @page = params['page'].to_i % @per_page == 0 ? params['page'].to_i : 0
+    @list ||= logged_in? ? current_user.todos.offset(@page).limit(@per_page).where(is_done: true).order(Arel.sql("is_done desc, (deadline - now()) asc")) : nil
+    @count = @list.count
     erb :'list/index'
   end
   
+  get "/missed" do
+    @per_page = 6
+    @page = params['page'].to_i % @per_page == 0 ? params['page'].to_i : 0
+    @list ||= logged_in? ? current_user.todos.offset(@page).limit(@per_page).where(is_done: false).where("deadline < now()").order(Arel.sql("is_done desc, (deadline - now()) asc")) : nil
+    @count = @list.count
+    erb :'list/index'
+  end
+
   def logged_in?
     !!current_user
   end
@@ -26,6 +46,10 @@ class ApplicationController < Sinatra::Base
   not_found do
     status 404
     erb :'404'
+  end
+
+  def h(html)
+    CGI.escapeHTML html
   end
   
 end
--- week9/todo/app/controllers/todo_controller.rb
+++ week9/todo/app/controllers/todo_controller.rb
@@ -5,13 +5,23 @@ class TodoController < ApplicationController
   end
 
   get '/todo/:id/edit' do
-    begin
-      todo ||= Todo.find_by(id: params["id"])
-    rescue ActiveRecord::RecordNotFound
-      not_found
-    end
+    @todo = todo_check(params['id'])
 
     erb :'list/edit'
+  end
+
+  put '/todo/:id/edit' do
+    result = TodoValidateService.new(params).call
+
+    unless result.empty? 
+      flash[:error_messages] = result
+      redirect("/todo/#{params['id']}/edit")
+    else
+      TodoUpdateService.new(params, current_user['id']).call 
+      flash[:success_message] = 'To Do Updated successfully'
+      redirect('/')
+    end
+
   end
 
   post '/todo' do
@@ -27,12 +37,29 @@ class TodoController < ApplicationController
     end
   end
 
-  patch '/todo' do
-    # .. update in db ..
+  get '/todo/:id/toggledone' do
+    todo_check(params['id'])
+    TodoDoneService.new(params, current_user, params['id']).call
+    flash[:success_message] = 'To Do done successfully'
+    redirect('/')
   end
 
-  delete '/todo' do
-    # .. delete todo item ..
+  delete '/todo/:id/delete' do
+    DeleteTodoService.new(params, current_user['id']).call
+    flash[:success_message] = 'To Do Deleted successfully'
+    redirect('/')
+  end
+
+
+  private 
+
+  def todo_check(id)
+    begin
+      todo ||= Todo.find(id)
+      return todo
+    rescue ActiveRecord::RecordNotFound
+      not_found
+    end
   end
 
 end
